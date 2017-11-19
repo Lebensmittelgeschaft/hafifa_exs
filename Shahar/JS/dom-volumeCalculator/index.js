0f8e4a0f94702875e68258b77004c76acab00134
shahar-y <shya95@gmail.com>
2017-11-19 13:16:04 +0200

    pushing my changes in hindsight

---
+++ Shahar/JS/dom-volumeCalculator/index.js
@@ -0,0 +1,9 @@
+
+function calculate(){
+
+        let list = document.getElementById("colorSelect");
+        color = document.getElementById("colorSelect").value;
+        console.log(list);
+        list.remove(list.selectedIndex);
+
+}
