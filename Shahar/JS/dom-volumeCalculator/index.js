e12abe9ac36769909f830e4c9e7e9068a748c23b
Shahar Yair <shya95@gmail.com>
2017-11-19 13:49:15 +0200

    manually added new ex.

--- Shahar/JS/dom-volumeCalculator/index.js
+++ Shahar/JS/dom-volumeCalculator/index.js
@@ -1,9 +1,13 @@
+//dom ex.9  https://www.w3resource.com/javascript-exercises/javascript-dom-exercises.php
+
 
 function calculate(){
 
-        let list = document.getElementById("colorSelect");
-        color = document.getElementById("colorSelect").value;
-        console.log(list);
-        list.remove(list.selectedIndex);
+        let radius = document.getElementById("radius").value;
+
+        console.log(radius);
+
+        document.getElementById("volume").innerHTML = Math.pow(radius,3)*(4/3)*Math.PI;
+        // pow(radius,3)
 
 }
