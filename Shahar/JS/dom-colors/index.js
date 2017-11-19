807a48637a15029091b16a8e8d5174b6375b1318
Shahar Yair <shya95@gmail.com>
2017-11-19 13:56:50 +0200

    added second DOM exercise manually. For some reason it was deleted last time.

---
+++ Shahar/JS/dom-colors/index.js
@@ -0,0 +1,9 @@
+//dom ex.9  https://www.w3resource.com/javascript-exercises/javascript-dom-exercises.php
+
+
+
+function removecolor()
+{
+    var x=document.getElementById("colorSelect");
+    x.remove(x.selectedIndex);
+}
